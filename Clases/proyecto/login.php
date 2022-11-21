a3e284618ef0a4afd2144942b5166976fbc6ed7b
Orlando <orlahdz50@gmail.com>
2022-11-21 15:18:58 -0600

    Se arreglo la parte del login y register
    Al momento de acceder al inicio de sesión y register la imagen no se
    encuentra ya que es una imagen que esta en el servidor, por lo tanto
    opte por quitarla y que mejor se centre el formulario de login y
    register.

--- Clases/proyecto/login.php
+++ Clases/proyecto/login.php
@@ -7,10 +7,10 @@
 <section class="vh-100">
   <div class="container py-5 h-100">
     <div class="row d-flex align-items-center justify-content-center h-100">
-      <div class="col-md-8 col-lg-7 col-xl-6">
+      <!-- <div class="col-md-8 col-lg-7 col-xl-6">
         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
           class="img-fluid" alt="Phone image">
-      </div>
+      </div> -->
       <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
         <h2>Inicio de sesión</h2>
         <br>
